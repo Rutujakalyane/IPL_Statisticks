119d1a392fc9c90629876a8025f17d59aba33427
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-05-21 11:51:21 +0530

    ADDED:test to perform most populous bowler by avg

---
+++ main/java/IPL/IPLRecoerdDAO.java
@@ -0,0 +1,40 @@
+package IPL;
+
+class IPLRecordDAO {
+
+    public IPLRecordDAO() {
+
+    }
+
+    public String player;
+    public int runs;
+    public double strikeRate;
+    public double average;
+    public int fours;
+    public int six;
+    public int wkts;
+    public int fourWkts;
+    public int fiveWkts;
+
+    public IPLRecordDAO(IplRunsCSV mostRunCSV) {
+        player = mostRunCSV.player;
+        runs = mostRunCSV.run;
+        strikeRate = mostRunCSV.strikeRate;
+        average = mostRunCSV.avg;
+        fours = mostRunCSV.fours;
+        six = mostRunCSV.six;
+    }
+
+   public IPLRecordDAO(String player, int runs, double strikeRate, double average, int fours, int six, int wkts, int fourWkts, int fiveWkts) {
+        this.player = player;
+        this.runs = runs;
+        this.strikeRate = strikeRate;
+        this.average = average;
+        this.fours = fours;
+        this.six = six;
+        this.wkts = wkts;
+        this.fourWkts = fourWkts;
+        this.fiveWkts = fiveWkts;
+    }
+}
+
