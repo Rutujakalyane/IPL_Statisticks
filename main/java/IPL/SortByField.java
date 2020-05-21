f759527421ba01fe4b2341b77929e75fd5384417
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-05-21 10:39:11 +0530

    REFACTORED:Enums for average with strike rate

--- main/java/IPL/SortByField.java
+++ main/java/IPL/SortByField.java
@@ -8,7 +8,8 @@ public class SortByField {
     static Map<Parameter, Comparator> sortParameterComparator = new HashMap<>();
 
     public enum Parameter {
-        AVG, STRIKERATE, CENTUARY, FOURS, HALFCENTUARY, HIGHSCORE, SIX, RUN , Six_AND_Fours,SIX_AND_FOURS_WITH_STRIKERATE;
+        AVG, STRIKERATE, CENTUARY, FOURS, HALFCENTUARY, HIGHSCORE, SIX, RUN , Six_AND_Fours,
+        SIX_AND_FOURS_WITH_STRIKERATE,AVG_WITH_STRIKERATE;
     }
 
     SortByField() {
